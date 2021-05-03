d66ef202e5e34a25cb1594923b396aa8bd8bd8c9
EstefaniEvangelista <estefanievangelista96@gmail.com>
2021-05-02 23:33:57 -0300

    exibindo apenas um item da lista e removendo um produto

---
+++ lista.py
@@ -0,0 +1,5 @@
+produtos = ["Sabonete", "Sabão em pó", "Detergente"]
+valoresProduto = ["1.50", "23.60", "3"]
+qtProduto = ["2", "10", "4"]
+produtos.remove('Sabonete')
+print("Produto:", produtos[1],"\n",  "R$:", valoresProduto[1], "\n", "Quantidade:",  qtProduto[1])
